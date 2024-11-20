44d2fb5608018ac4d7ec6eb6a167b7d07be0d7a7
DYM5 <dym5.official@gmail.com>
2024-11-20 14:41:32 +0545

    Update .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,16 +1,26 @@
-type Connection = {
-    id: string,
-    unlink: () => void
+declare module "wirec" {
+    type Callback = (...args: any[]) => void;
+
+    interface Connection {
+        unlink: () => void;
+        id: string;
+    }
+
+    interface Wirec {
+        on(event_key: string, callback: Callback): Connection;
+        onx(event_key: string, callback_id: string): void;
+        ons(event_key: string, callback: Callback): Connection;
+        put(event_key: string, ...args: any[]): void;
+        hook(key: string): (...args: any[]) => void;
+
+        state: {
+            init<T>(name: string, useState: (initialValue: T) => [T, (value: T) => void], initialValue: T): [T, (value: T) => void];
+            set<T>(name: string, value: T): void;
+            get<T>(name: string): T;
+        };
+    }
+
+    const wirec: Wirec;
+
+    export = wirec;
 }
-
-type Wirec = {
-    on(event_key: string, callback: (...args: any[]) => void): Connection;
-    onx(event_key: string, callback_id: string): void;
-    ons(event_key: string, callback: (...args: any[]) => void): Connection;
-    put(event_key: string, ...args: any[]): void;
-    hook(event_key: string): (...args) => void;
-}
-
-declare const wirec: Wirec;
-
-export default wirec;
